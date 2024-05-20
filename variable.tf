46e61a046ff124e0d0190cb4a9aa93693369d3cb
aziz200115 <awekerbekov@gmail.com>
2024-05-20 18:41:33 -0500

    created example files

--- variable.tf
+++ variable.tf
@@ -1,5 +1,18 @@
-variable name {
-    description = "Please provide name"
-    type = string
-    default = ""
+variable "name" {
+  description = "Please provide name"
+  type        = string
+  default     = ""
+}
+
+
+variable "annotations" {
+  description = "Please proovide annotations"
+  type = map
+  default = {}  
+}
+
+variable "labels" {
+  description = "Please proovide labels"
+  type = map
+  default = {} 
 }
